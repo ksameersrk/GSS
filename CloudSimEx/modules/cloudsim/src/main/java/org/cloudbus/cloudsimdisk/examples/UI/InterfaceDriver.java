a1fb2daf07109eb797955eafdf2365dbe89a1908
SaiVishwas <saivishwasp@gmail.com>
2017-04-08 17:58:35 +0530

    taking command line args

--- CloudSimEx/modules/cloudsim/src/main/java/org/cloudbus/cloudsimdisk/examples/UI/InterfaceDriver.java
+++ CloudSimEx/modules/cloudsim/src/main/java/org/cloudbus/cloudsimdisk/examples/UI/InterfaceDriver.java
@@ -13,6 +13,7 @@ public class InterfaceDriver {
         // staging disk properties
         boolean addStagingDisk = Boolean.parseBoolean(args[1]);
 
+        /*
         int numberOfOperations = 10;
         String distribution = "read intensive";
 
@@ -24,6 +25,19 @@ public class InterfaceDriver {
         String cachingMechanism = "LRU"; // FIFO also possible
         int diskType = 1; // basicallly this number is the id for storage and power model, will assign ids to them
         //Scenarios : this part is to be done in front end
+        */
+
+        int numberOfOperations = Integer.parseInt(args[2]);
+        String distribution = args[3];
+
+        // will have a set of predefined workloads , user selects one of them,
+        // predefindedWorkloadNumber variable stores the workload id
+        int predefindedWorkloadNumber = Integer.parseInt(args[4]);
+
+        int noOfReplicas = Integer.parseInt(args[5]); //default 3
+        String cachingMechanism = args[5]; // FIFO also possible
+        int diskType = Integer.parseInt(args[6]); // basicallly this number is the id for storage and power model, will assign ids to them
+
         startSimulation(totalNoOfNodes, addStagingDisk, numberOfOperations, predefindedWorkloadNumber, noOfReplicas, cachingMechanism, diskType);
     }
 }
