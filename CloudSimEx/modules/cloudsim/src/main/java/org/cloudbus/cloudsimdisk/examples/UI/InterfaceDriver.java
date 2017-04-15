34ab8b36a2051b6420095435c47022bbc20a7c97
SaiVishwas <saivishwasp@gmail.com>
2017-04-15 23:14:27 +0530

    returning json for all scenarios, for all metrics

--- CloudSimEx/modules/cloudsim/src/main/java/org/cloudbus/cloudsimdisk/examples/UI/InterfaceDriver.java
+++ CloudSimEx/modules/cloudsim/src/main/java/org/cloudbus/cloudsimdisk/examples/UI/InterfaceDriver.java
@@ -6,6 +6,7 @@ import org.cloudbus.cloudsimdisk.examples.MyRunner;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 import static org.cloudbus.cloudsimdisk.examples.SimulationScenarios.FlushEntireStagingDiskContents.startSimulation;
@@ -74,7 +75,7 @@ public class InterfaceDriver {
         String pathToInputLog = "files/basic/operations/idealInputLog.txt";
         boolean generateInputLog = false;
 
-        int scenario = 1;
+        int scenario = 3;
         if(scenario == 1){
             addStagingDisk = false;
             MyRunner runner = startSimulation(totalNoOfNodes, addStagingDisk, numberOfOperations, predefindedWorkloadNumber, noOfReplicas, cachingMechanism,
@@ -85,58 +86,171 @@ public class InterfaceDriver {
             List<Map<String,Object>> diskStats = runner.getDiskStats();
             //System.out.println(diskStats.toString());
 
-            Collections.sort(diskStats, new Comparator<Map<String, Object>>() {
-                public int compare(final Map<String, Object> o1, final Map<String, Object> o2) {
-                    int s1 = Integer.parseInt(o1.get("disk name").toString());
-                    int s2 = Integer.parseInt(o2.get("disk name").toString());
-                    return s1 - s2;
-                }
-            });
+            getSortedAndDiskNameChangedDiskStats(diskStats);
 
-            Map<String, Object> graphJson = new HashMap<>();
-            List<String> xAxisLabels = new ArrayList<>();
-            List<Double> yAxisLabel = new ArrayList<>();
-            for(Map<String,Object> map : diskStats){
-
-                if(Integer.parseInt(map.get("disk name").toString()) >= 1000){
-                    map.put("disk name", "SSD" +map.get("disk name").toString());
-                } else {
-                    map.put("disk name", "HDD" +map.get("disk name").toString());
-                }
-                xAxisLabels.add(map.get("disk name").toString());
-                yAxisLabel.add(Double.parseDouble(map.get("total energy").toString()));
-
-
-
-            }
-
-            List<List<Double>> data = new ArrayList<>();
-            List<String> series = new ArrayList<>();
-            series.add("with staging disk");
-            data.add(yAxisLabel);
-            graphJson.put("label", xAxisLabels);
-            graphJson.put("data", yAxisLabel);
-            graphJson.put("series", series);
-
-
-            Gson gson = new Gson();
-            //gson.toJson(diskStats, new FileWriter("/Users/spadigi/Desktop/greenSwiftSimulation/GSS/server/data/dataStat.json"));
-            String jsonInString = gson.toJson(graphJson);
-            //System.out.println(jsonInString);
-            FileUtils.writeStringToFile(new File("/Users/spadigi/Desktop/greenSwiftSimulation/GSS/server/data/line_chart.json"), jsonInString);
+            drawLineGraph(diskStats, "without staging disk");
         }
         else if(scenario == 2) {
             addStagingDisk = true;
             MyRunner runner = startSimulation(totalNoOfNodes, addStagingDisk, numberOfOperations, predefindedWorkloadNumber, noOfReplicas, cachingMechanism,
                     HDDType, SSDType,
                     percentageFlushAt, percentageFlushTill, realisticSSD, pathToWorkload, pathToStartingFileList, pathToInputLog, generateInputLog);
+
+            List<Map<String,Object>> diskStats = runner.getDiskStats();
+            //System.out.println(diskStats.toString());
+            drawLineGraph(diskStats, "with staging disk");
+
         }
         else if(scenario == 3){
 
+            addStagingDisk = false;
+            MyRunner runner = startSimulation(totalNoOfNodes, addStagingDisk, numberOfOperations, predefindedWorkloadNumber, noOfReplicas, cachingMechanism,
+                    HDDType,
+                    SSDType,
+                    percentageFlushAt, percentageFlushTill, realisticSSD, pathToWorkload, pathToStartingFileList, pathToInputLog, generateInputLog);
+
+            List<Map<String,Object>> diskStats = runner.getDiskStats();
+            //System.out.println(diskStats.toString());
+            getSortedAndDiskNameChangedDiskStats(diskStats);
+
+            addStagingDisk = true;
+            MyRunner runnerSSD = startSimulation(totalNoOfNodes, addStagingDisk, numberOfOperations, predefindedWorkloadNumber, noOfReplicas, cachingMechanism,
+                    HDDType, SSDType,
+                    percentageFlushAt, percentageFlushTill, realisticSSD, pathToWorkload, pathToStartingFileList, pathToInputLog, generateInputLog);
+
+            List<Map<String,Object>> diskStatsSSD = runnerSSD.getDiskStats();
+            //System.out.println(diskStats.toString());
+
+            getSortedAndDiskNameChangedDiskStats(diskStatsSSD);
+
+            //List<String> xAxisLabels = getXaxisLabels(diskStats, "disk name");
+            List<Double> yAxisLabelTotalPowerSSD = getYaxisLabels(diskStats, "total energy");
+            List<Double> yAxisLabelIdleTimeSSD = getYaxisLabels(diskStats, "idle time");
+            List<Double> yAxisLabelActiveTimeSSD = getYaxisLabels(diskStats,"active time" );
+            List<Double> yAxisLabelIdleEnergySSD = getYaxisLabels(diskStats, "idle energy");
+            List<Double> yAxisLabelActiveEnergySSD = getYaxisLabels(diskStats, "active energy");
+
+            List<String> xAxisLabels = getXaxisLabels(diskStats, "disk name");
+            List<Double> yAxisLabelTotalPower = getYaxisLabels(diskStats, "total energy");
+            List<Double> yAxisLabelIdleTime = getYaxisLabels(diskStats, "idle time");
+            List<Double> yAxisLabelActiveTime = getYaxisLabels(diskStats,"active time" );
+            List<Double> yAxisLabelIdleEnergy = getYaxisLabels(diskStats, "idle energy");
+            List<Double> yAxisLabelActiveEnergy = getYaxisLabels(diskStats, "active energy");
+
+            Map<String, Object> graphJsonTotalPower = getLineGraphJSON(xAxisLabels,yAxisLabelTotalPower,yAxisLabelTotalPowerSSD,"without staging disk",
+                    "with staging disk");
+            Map<String, Object> graphJsonIdleTime = getLineGraphJSON(xAxisLabels,yAxisLabelIdleTime,yAxisLabelIdleTimeSSD,"without staging disk","with staging disk");
+            Map<String, Object> graphJsonActiveTime = getLineGraphJSON(xAxisLabels,yAxisLabelActiveTime,yAxisLabelActiveTimeSSD,"without staging disk", "with staging disk");
+            Map<String, Object> graphJsonIdleEnergy = getLineGraphJSON(xAxisLabels,yAxisLabelIdleEnergy,yAxisLabelIdleEnergySSD,"without staging disk", "with staging disk");
+            Map<String, Object> graphJsonActiveEnergy = getLineGraphJSON(xAxisLabels,yAxisLabelActiveEnergy,yAxisLabelActiveEnergySSD,"without staging disk", "with staging disk");
+
+            dumpToFile(graphJsonTotalPower, "line_chart_total_power");
+            dumpToFile(graphJsonIdleTime,"line_chart_idle_time");
+            dumpToFile(graphJsonActiveTime,"line_chart_active_time");
+            dumpToFile(graphJsonIdleEnergy,"line_chart_idle_energy");
+            dumpToFile(graphJsonActiveEnergy,"line_chart_active_energy");
+
+
+
         }
 
 
 
 
+    }
+
+    public static void dumpToFile(Map<String, Object> graphJson, String filename) throws IOException{
+        Gson gson = new Gson();
+        String jsonInString = gson.toJson(graphJson);
+        String path = "/Users/spadigi/Desktop/greenSwiftSimulation/GSS/server/data/" + filename + ".json";
+        FileUtils.writeStringToFile(new File(path), jsonInString);
+    }
+
+    public static void drawLineGraph(List<Map<String,Object>> diskStats, String seriesName) throws IOException{
+
+        List<String> xAxisLabels = getXaxisLabels(diskStats, "disk name");
+        List<Double> yAxisLabelTotalPower = getYaxisLabels(diskStats, "total energy");
+        List<Double> yAxisLabelIdleTime = getYaxisLabels(diskStats, "idle time");
+        List<Double> yAxisLabelActiveTime = getYaxisLabels(diskStats,"active time" );
+        List<Double> yAxisLabelIdleEnergy = getYaxisLabels(diskStats, "idle energy");
+        List<Double> yAxisLabelActiveEnergy = getYaxisLabels(diskStats, "active energy");
+
+        Map<String, Object> graphJsonTotalPower = getLineGraphJSON(xAxisLabels,yAxisLabelTotalPower,seriesName);
+        Map<String, Object> graphJsonIdleTime = getLineGraphJSON(xAxisLabels,yAxisLabelIdleTime,seriesName);
+        Map<String, Object> graphJsonActiveTime = getLineGraphJSON(xAxisLabels,yAxisLabelActiveTime,seriesName);
+        Map<String, Object> graphJsonIdleEnergy = getLineGraphJSON(xAxisLabels,yAxisLabelIdleEnergy,seriesName);
+        Map<String, Object> graphJsonActiveEnergy = getLineGraphJSON(xAxisLabels,yAxisLabelActiveEnergy,seriesName);
+
+        dumpToFile(graphJsonTotalPower, "line_chart_total_power");
+        dumpToFile(graphJsonIdleTime,"line_chart_idle_time");
+        dumpToFile(graphJsonActiveTime,"line_chart_active_time");
+        dumpToFile(graphJsonIdleEnergy,"line_chart_idle_energy");
+        dumpToFile(graphJsonActiveEnergy,"line_chart_active_energy");
+    }
+
+    public static Map<String, Object> getLineGraphJSON(List<String> xAxisLabels, List<Double> yAxisLabels, String series_name){
+        Map<String, Object> graphJson = new HashMap<>();
+        List<List<Double>> data = new ArrayList<>();
+        List<String> series = new ArrayList<>();
+        series.add(series_name);
+        data.add(yAxisLabels);
+        graphJson.put("label", xAxisLabels);
+        graphJson.put("data", data);
+        graphJson.put("series", series);
+
+        return graphJson;
+    }
+
+    public static Map<String, Object> getLineGraphJSON(List<String> xAxisLabels, List<Double> yAxisLabels0, List<Double> yAxisLabels1, String series_name0,
+                                                       String series_name1){
+        Map<String, Object> graphJson = new HashMap<>();
+        List<List<Double>> data = new ArrayList<>();
+        List<String> series = new ArrayList<>();
+        series.add(series_name0);
+        series.add(series_name1);
+        data.add(yAxisLabels0);
+        data.add(yAxisLabels1);
+        graphJson.put("label", xAxisLabels);
+        graphJson.put("data", data);
+        graphJson.put("series", series);
+
+        return graphJson;
+    }
+
+
+    public static void getSortedAndDiskNameChangedDiskStats(List<Map<String,Object>> diskStats){
+        Collections.sort(diskStats, new Comparator<Map<String, Object>>() {
+            public int compare(final Map<String, Object> o1, final Map<String, Object> o2) {
+                int s1 = Integer.parseInt(o1.get("disk name").toString());
+                int s2 = Integer.parseInt(o2.get("disk name").toString());
+                return s1 - s2;
+            }
+        });
+
+        for(Map<String,Object> map : diskStats){
+
+            if(Integer.parseInt(map.get("disk name").toString()) >= 1000){
+                map.put("disk name", "SSD" +map.get("disk name").toString());
+            } else {
+                map.put("disk name", "HDD" +map.get("disk name").toString());
+            }
+
+        }
+    }
+
+    public static List<Double> getYaxisLabels(List<Map<String,Object>> diskStats, String metric) {
+        List<Double> yAxisLabel = new ArrayList<>();
+        for(Map<String,Object> map : diskStats){
+            yAxisLabel.add(Double.parseDouble(map.get(metric).toString()));
+        }
+        return yAxisLabel;
+    }
+
+    public static List<String> getXaxisLabels(List<Map<String,Object>> diskStats, String metric) {
+        List<String> xAxisLabel = new ArrayList<>();
+        for(Map<String,Object> map : diskStats){
+            xAxisLabel.add(map.get(metric).toString());
+        }
+        return xAxisLabel;
     }
 }
